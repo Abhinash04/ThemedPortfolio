5d3d336b22c89bc8508d207acdf0dcc03d068608
abhinash <abhinash@gravityer.com>
2026-03-17 16:57:11 +0530

    [feat]: added finder app along with txt and img viewer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Terminal from "@/components/apps/Terminal"
 import Skills from "@/components/apps/Skills"
 import Safari from "@/components/apps/Safari"
 import Resume from "@/components/apps/Resume"
+import Finder from "@/components/apps/Finder"
+import TextViewer from "@/components/apps/TextViewer"
+import ImageViewer from "@/components/apps/ImageViewer"
 
 const App = () => {
   return (
@@ -14,6 +17,9 @@ const App = () => {
       <Safari />
       <Dock />
       <Resume />
+      <Finder />
+      <TextViewer />
+      <ImageViewer />
     </main>
   )
 }
