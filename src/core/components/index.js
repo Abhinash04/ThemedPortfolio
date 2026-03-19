86a8599385df079982cbb0aee74bd9b91d3432e1
abhinash <abhinash@gravityer.com>
2026-03-19 16:31:26 +0530

    [feat]: home page created

--- src/core/components/index.js
+++ src/core/components/index.js
@@ -1,2 +1,3 @@
 export { default as windowWrapper } from "@/core/components/windowWrapper";
 export { default as instanceWrapper } from "@/core/components/instanceWrapper";
+export { default as Home } from "@/core/components/Home";
